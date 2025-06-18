74b94b1f8e24db83ec479d6839331df20023560d
Robinator9002 <r.schumi09@gmail.com>
2025-06-18 07:21:36 +0200

    Change the Path of the Files

---
+++ Source/ChaosRifts/Private/Core/ChaosRiftsGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Robinator Studios, Inc. All Rights Reserved.
+
+#include "ChaosGameMode.h"
+
+AChaosGameMode::AChaosGameMode()
+{
+	// stub
+}
---
+++ Source/ChaosRifts/Public/Core/ChaosGameMode.h
@@ -0,0 +1,21 @@
+// Copyright Robinator Studios, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChaosGameMode.generated.h"
+
+/**
+ *  Simple GameMode for a third person game
+ */
+UCLASS(abstract)
+class AChaosGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	
+	/** Constructor */
+	AChaosGameMode();
+};
