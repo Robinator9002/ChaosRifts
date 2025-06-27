7326ebd17822cff2603f08690789df34a545d05c
Robinator9002 <r.schumi09@gmail.com>
2025-06-27 09:34:16 +0200

    Set back Item Class to avoid some errors from the Compiling

--- Source/ChaosRifts/Public/Items/Item.h
+++ Source/ChaosRifts/Public/Items/Item.h
@@ -6,11 +6,14 @@
 #include "GameFramework/Actor.h"
 #include "Item.generated.h"
 
-class UStaticMeshComponent;
+class UCapsuleComponent;
+
+// Delegate that is triggered when the overlap status of an item changes.
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnItemOverlapSignature, AActor*, OverlappedActor, bool, bIsOverlapping);
 
 /**
  * AItem is the base class for all pickup-able or interactive objects in the world.
- * It now uses a Static Mesh as its root for visuals and simple collision.
+ * It implements a generic collision detection mechanic with multiple hit capsules.
  */
 UCLASS(Blueprintable)
 class CHAOSRIFTS_API AItem : public AActor
@@ -20,7 +23,45 @@ class CHAOSRIFTS_API AItem : public AActor
 public:	
 	AItem();
 
-	// The root component of the item, which provides the visual representation and collision.
+	// The root component of the item. Often a StaticMesh or SkeletalMesh.
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item")
-	TObjectPtr<UStaticMeshComponent> ItemMesh;
+	TObjectPtr<USceneComponent> SceneRoot;
+	
+	// Delegate that is called when an Actor enters or leaves one of the hit capsules.
+	UPROPERTY(BlueprintAssignable, Category = "Item|Events")
+	FOnItemOverlapSignature OnItemOverlap;
+
+	/**
+	 * Adds a new hit capsule to this item.
+	 * This is useful for defining complex collision shapes in Blueprints or derived classes.
+	 * @param CapsuleToAdd The capsule to be added to the array.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Item|Collision")
+	void AddHitCapsule(UCapsuleComponent* CapsuleToAdd);
+
+	/** * Returns all actors that are currently overlapping with THIS item. 
+	 * Renamed from GetOverlappingActors to avoid conflict with the base AActor function.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Item|Collision")
+	const TArray<AActor*>& GetItemOverlappingActors() const { return OverlappingActors; }
+
+protected:
+	virtual void BeginPlay() override;
+
+	// Array for storing all actors that are currently overlapping with one of the hit capsules.
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Item|State")
+	TArray<TObjectPtr<AActor>> OverlappingActors;
+
+private:
+	// Functions that are bound to the OnComponentBeginOverlap and OnComponentEndOverlap delegates of the capsules.
+	UFUNCTION()
+	void OnHitCapsuleBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnHitCapsuleEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	// An array containing all capsules responsible for collision for this item.
+	// These must be created and added in the derived class or Blueprint.
+	UPROPERTY(VisibleAnywhere, Category = "Item|Collision")
+	TArray<TObjectPtr<UCapsuleComponent>> HitCapsules;
 };
